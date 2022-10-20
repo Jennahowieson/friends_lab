2f45a9afd0ec904a576f6783e962297e3e01e9cb
Jenna Howieson <jenna@jennahowieson.co.uk>
2022-10-20 16:20:44 +0100

    partially completed lab

---
+++ start_point/src/friends.py
@@ -0,0 +1,18 @@
+
+#  def test_add(self):
+#       add_result = add( 1, 2 )
+#       self.assertEqual( 3, add_result )
+
+# def add(num_1, num_2):
+#     return num_1 + num_2
+
+# --------------------------------------
+#  def test_getting_name(self):
+#     result = get_name(self.person5)
+#     self.assertEqual("Daphne", result)
+
+from re import X
+
+
+def getting_name(person):
+result = person ["name"]
